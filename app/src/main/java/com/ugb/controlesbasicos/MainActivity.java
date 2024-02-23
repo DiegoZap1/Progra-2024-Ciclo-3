d7f9a7c4adb3974e06eb5c4b23c6a7b2b0008a4e
DiegoZap <zapatadp308@gmail.com>
2024-02-23 11:10:54 -0600

    Parcial 1

--- app/src/main/java/com/ugb/controlesbasicos/MainActivity.java
+++ app/src/main/java/com/ugb/controlesbasicos/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     TextView tempVal;
     Spinner spn;
     Button btn;
-    conversores objConversor = new conversores();
+    conversor objParcial = new conversor();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +24,64 @@ public class MainActivity extends AppCompatActivity {
 
         tbh = findViewById(R.id.tbhParcial);
         tbh.setup();
-        tbh.addTab(tbh.newTabSpec("LON").setIndicator("LONGITUD", null).setContent(R.id.tabLogitud));
+        tbh.addTab(tbh.newTabSpec("TAR").setIndicator("TARIFA AGUA", null).setContent(R.id.tabTarifaAgua));
+        tbh.addTab(tbh.newTabSpec("ARE").setIndicator("AREA", null).setContent(R.id.tabArea));
 
+        btn=findViewById(R.id.btnCalcularAgua);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tempVal=findViewById(R.id.txtCantidadAgua);
+                double cantidad = Double.parseDouble(tempVal.getText().toString());
+
+                double cuotafija = 6.0;
+                double tasaExceso = 0.45;
+                double sobreExceso = 0.65;
+
+                double ValorAPagar;
+
+                if (cantidad <= 18){
+                    ValorAPagar = cuotafija;
+                } else if (cantidad <= 28){
+                    double exceso = cantidad - 18;
+                    double cargoExceso = exceso * tasaExceso;
+                    ValorAPagar = cuotafija + cargoExceso;
+                } else{
+                    double exceso = cantidad - 28;
+                    double cargaExceso = exceso * sobreExceso;
+                    ValorAPagar = cuotafija + cargaExceso;
+                }
+
+                Toast.makeText(getApplicationContext(), "Respuesta: "+ ValorAPagar, Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+        btn = findViewById(R.id.btnCalcularArea);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                spn = findViewById(R.id.spndeArea);
+                int de = spn.getSelectedItemPosition();
+
+                spn = findViewById(R.id.spnAArea);
+                int a = spn.getSelectedItemPosition();
+
+                tempVal = findViewById(R.id.txtCantidadArea);
+                double cantidad= Double.parseDouble(tempVal.getText().toString());
+                double resp = objParcial.convertirArea(0, de, a, cantidad);
+
+                Toast.makeText(getApplicationContext(), "Respuesta: "+ resp, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
+class conversor{
+    double[][] valoresArea = {
+            {1,10.763,1.43,1.19599,0.0022883295194508,0.000143082804880,0.0001}
+    };
+
+    public double convertirArea(int opcion, int de, int a, double cantidad){
+        return valoresArea[opcion][a] / valoresArea[opcion][de] * cantidad;
+    }
 }
